90821eff0e29d826db693fba3d1bd8cd6b546cfc
thinhnguyen347 <thinh.ohg@gmail.com>
2021-04-09 10:49:22 +0700

    fix

---
+++ B19_QuizGame/script19.js
@@ -0,0 +1,25 @@
+let questions = [
+    {
+        question: "Name a mammal that cannot jump",
+        answer: ["grasshopper", "frog", "kangaroo", "elephant"],
+        correctAns: "elephant",
+    },
+
+    {
+        question: "What is the fastest land animal?",
+        answer: ["cheetah", "lion", "snake", "kangaroo"],
+        correctAns: "cheetah",
+    },
+
+    {
+        question: "What is the sweet food made by bees?",
+        answer: ["chocolate", "pollen", "honey", "flower"],
+        correctAns: "honey",
+    },
+
+    {
+        question: "On a common ladybug &#comma; what color are its spots?",
+        answer: ["red", "black", "yellow", "white"],
+        correctAns: "black",
+    },
+]
