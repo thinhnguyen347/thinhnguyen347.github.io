064353676b79bbc86dfd0bf6c1e46c5047fb4022
thinhnguyen347 <thinh.ohg@gmail.com>
2021-04-15 18:24:25 +0700

    bt

--- web-responsive/js/main7.js
+++ web-responsive/js/main7.js
@@ -1,12 +1,6 @@
 
-if (window.matchMedia && window.matchMedia('(max-width: 600px)').matches) {
-    $("div.show-menu").addClass("hide");
+if (window.matchMedia && window.matchMedia('(max-width: 500px)').matches) {
+    $("div.show-menu").addClass("hide")}
+    
     $("img").click(function () {
-        if ($("div.show-menu").hasClass("hide")) {
-            $("div.show-menu").removeClass("hide")
-        } else { $("div.show-menu").addClass("hide") }
-    })
-}
-
-
-
+    $("div.show-menu").toggleClass("hide")})
