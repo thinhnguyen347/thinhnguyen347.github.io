206c56e25eff092c6bd71eff63658d47df63fbae
thinhnguyen347 <thinh.ohg@gmail.com>
2021-05-20 13:02:05 +0700

    bt

--- b27_quanlyhocvien/js/main.js
+++ b27_quanlyhocvien/js/main.js
@@ -3,13 +3,18 @@ $('.btn-add').click(function () {
     $('.modal-add').removeClass('hide');
 })
 
-// Tắt Modal thêm học sinh
-$('.fa-times').click(function () {
-    $('.modal-add').addClass('hide');
-})
+// Lấy dữ liệu từ form nhập-----------------------------------------------------------------
+// let new_avatar = $(".input-data #avatar").val();
+let new_name = $('.input-data #name').val();
+let new_dob = $('.input-data #dob').val();
+let new_gender = $('.input-data input[name="gender"]:checked').val();
+let new_address = $('.input-data #DiaChi').val();
+let new_email = $('.input-data #email').val();
+let new_phone = $('.input-data #sdt').val();
 
-// Lấy dữ liệu từ form nhập
 $('button.submit').click(function () {
+
+    $('.modal-add').addClass('hide');
     $('.item-list').append(
         '<div class="item">' +
         '<div class="icon"><i class="far fa-edit"></i></div>' +
@@ -17,25 +22,24 @@ $('button.submit').click(function () {
         '<img src="img/student10.png" alt="student-image">' +
         '</div>' +
         '<div class="details">' +
-        '                    <h3 class="HoTen">Yaya Urassaya</h3>' +
+        '<h3 class="HoTen">' + new_name + '</h3>' +
         '<ul>' +
-        '                        <li>Ngày sinh: <span class="NgaySinh">18/03/1993</span></li>' +
-        '                        <li>Giới tính: <span class="GioiTinh">Nữ</span></li>' +
-        '                        <li>Địa chỉ: <span class="DiaChi">Pattaya, Thái Lan</span></li>' +
-        '< li > E - mail: <span class="Email">123@gmail.com</span></li >' +
-        '<li>Số điện thoại: <span class="sdt">04 1900 8090</span></li>' +
-        '                    </ul >' +
+        '<li>Ngày sinh: <span class="NgaySinh">' + new_dob + '</span></li>' +
+        '<li>Giới tính: <span class="GioiTinh">' + new_gender + '</span></li>' +
+        '<li>Địa chỉ: <span class="DiaChi">' + new_address + '</span></li>' +
+        '<li> E - mail: <span class="Email">' + new_email + '</span></li >' +
+        '<li>Số điện thoại: <span class="sdt">' + new_phone + '</span></li>' +
+        '</ul >' +
         '</div >' +
         '</div >'
     )
 })
 
-// Nút Sửa/Xoá học viên
+// Nút Sửa/Xoá học viên--------------------------------------------------------------------
 
 $('.fa-edit').click(function () {
     $('.modal-edit').removeClass('hide');
 
-    // Lấy info của item được chọn:
     let item = $(this).parent().parent();
 
     // Lấy avatar
@@ -45,13 +49,20 @@ $('.fa-edit').click(function () {
     // Lấy thông tin tên
     let current_name = item.find('.details h3').text();
     $('.edit-data #name').val(current_name);
-    
+
     // Lấy thông tin ngày sinh
     let current_dob = item.find('.NgaySinh').text();
     $('.edit-data #dob').val(current_dob);
-    
-    // Lấy thông tin giới tính
 
+    // Lấy thông tin giới tính
+    let gender = item.find('.GioiTinh').text();
+    if (gender == "Nam") {
+        $('.edit-data #male').attr('checked', 'checked');
+    } else if (gender == "Nữ") {
+        $('.edit-data #female').attr('checked', 'checked');
+    } else {
+        $('.edit-data #other').attr('checked', 'checked');
+    }
 
     // Lấy thông tin địa chỉ
     let current_address = item.find('.DiaChi').text();
@@ -64,14 +75,36 @@ $('.fa-edit').click(function () {
     // Lấy thông tin sdt
     let current_phone = item.find('.sdt').text();
     $('.edit-data #sdt').val(current_phone);
-
-
-
 })
 
 
-
-// Tắt Modal edit thông tin
+// Tắt Modal thêm/edit thông tin
 $('.fa-times').click(function () {
     $('.modal-edit').addClass('hide');
+    $('.modal-add').addClass('hide');
 })
+
+// Tắt Modal xoá item -------------------------------------------------------------------------------
+$('.confirm').click(function () {
+    $('.modal-delete').addClass('hide');
+    
+})
+
+$('.cancel').click(function () {
+    $('.modal-delete').addClass('hide');
+})
+
+//Mở modal xác nhận xoá item
+$('.delete').click(function () {
+    $('.modal-edit').addClass('hide');
+    $('.modal-delete').removeClass('hide');
+})
+
+//Cập nhật thông tin item----------------------------------------------------------------------------
+
+
+// Nút Update & Tắt/bật thông báo chỉnh sửa thành công
+// $(".update").click(function () {
+//     $('.modal-edit').addClass('hide');
+// })
+
