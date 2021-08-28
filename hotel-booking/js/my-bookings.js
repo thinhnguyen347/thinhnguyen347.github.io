18142791dd25a0418947b4e4cbc7c5b3c77cd2e0
thinhnguyen347 <thinh.ohg@gmail.com>
2021-08-28 16:35:01 +0700

    update

--- hotel-booking/js/my-bookings.js
+++
@@ -1,6 +0,0 @@
-// Log-out
-$('.log-out').on('click', function () {
-       setTimeout(function(){
-        window.location.href='index.html'
-    }, 3500)
-  })
--- hotel-booking/js/updating.js
+++
@@ -1,2 +0,0 @@
-let logInStatus = localStorage.logIn;
-checkLoginStatus(logInStatus);
